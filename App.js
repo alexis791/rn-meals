76125e33de1abb4aa82266094e22d816c8b7b8ae
Alexis Delgado <dalexisbr@gmail.com>
2020-11-23 13:44:09 -0700

    Installing expo-font

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
+
+const fetchFonts = () => {
+	Font.loadAsync({
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+		'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf')
+	})
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontLoaded, setFontLoaded] = useState(false)
+
+	if (!fontLoaded) {
+		;<AppLoading
+			startAsync={fetchFonts}
+			onFinish={() => setFontLoaded(true)}
+		/>
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
