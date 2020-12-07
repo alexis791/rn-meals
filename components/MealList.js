b5bb928ccfd8aab179bf7acb28646d6c519c63b0
Alexis Delgado <dalexisbr@gmail.com>
2020-12-07 15:48:16 -0700

    Adding logic to favorites meals section

--- components/MealList.js
+++ components/MealList.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import { FlatList, StyleSheet, View } from 'react-native'
+import { useSelector } from 'react-redux'
 
 import MealItem from './MealItem'
 
 const MealList = (props) => {
-	const renderMealItem = (itemData) => (
-		<MealItem
-			title={itemData.item.title}
-			image={itemData.item.imageUrl}
-			duration={itemData.item.duration}
-			complexity={itemData.item.complexity}
-			affordability={itemData.item.affordability}
-			onSelectMeal={() => {
-				props.navigation.navigate({
-					routeName: 'MealDetail',
-					params: {
-						mealId: itemData.item.id
-					}
-				})
-			}}
-		/>
-	)
+	const favoriteMeals = useSelector((state) => state.meals.favoriteMeals)
+
+	const renderMealItem = (itemData) => {
+		const isFavorite = favoriteMeals.find(
+			(meal) => meal.id === itemData.item.id
+		)
+		return (
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {
+					props.navigation.navigate({
+						routeName: 'MealDetail',
+						params: {
+							mealId: itemData.item.id,
+							mealTitle: itemData.item.title,
+							isFav: isFavorite
+						}
+					})
+				}}
+			/>
+		)
+	}
 	return (
 		<View style={styles.list}>
 			<FlatList
--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { View, StyleSheet } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { useSelector } from 'react-redux'
 
 import HeaderButton from '../components/HeaderButton'
 import MealList from '../components/MealList'
-import { MEALS } from '../data/dummy-data'
+import DefaultText from '../components/DefaultText'
 
 const FavoriteScreen = (props) => {
-	const favMeals = MEALS.filter(
-		(meal) => meal.id === 'm1' || meal.id === 'm2'
-	)
+	const favMeals = useSelector((state) => state.meals.favoriteMeals)
+
+	if (favMeals.length === 0) {
+		return (
+			<View style={styles.screen}>
+				<DefaultText>
+					No favorite meals found. Start adding some!
+				</DefaultText>
+			</View>
+		)
+	}
+
 	return <MealList listData={favMeals} navigation={props.navigation} />
 }
 
@@ -28,5 +39,13 @@ FavoriteScreen.navigationOptions = (navData) => {
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+})
 
 export default FavoriteScreen
